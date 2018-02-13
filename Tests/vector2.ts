e3e4420616b4373034b708dc0aac438bc5765249
Rupert McKay <maxfmckay@gmail.com>
2018-02-13 08:22:28 +0000

    Update directory structure to put source files in common folder. Update html message 'BLOBS OF SHY AVOIDANCE'. Restructure distance unit test for readability

--- Tests/vector2.ts
+++ Tests/vector2.ts
@@ -1,14 +1,16 @@
 import * as chai from 'chai';
 import * as mocha from 'mocha';
-import { Vector2 } from '../vector2';
+import { Vector2 } from '../src/vector2';
 
 const expect = chai.expect;
 
 describe('Vector2', () => {
-    it('computes distance', () => {
-        var v0 = new Vector2(0, 0);
-        var v1 = new Vector2(3, 4);
-        var distance = v0.distance(v1);
-        expect(distance).to.equal(5);
+    describe('distance', () => {
+        it('computes 3-4-5 triangle', () => {
+            var v0 = new Vector2(0, 0);
+            var v1 = new Vector2(3, 4);
+            var distance = v0.distance(v1);
+            expect(distance).to.equal(5);
+        });
     });
 });
