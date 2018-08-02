d2585c29a5ed1d62f1ec9182f4266121f77c5566
Rupert McKay <maxfmckay@gmail.com>
2018-08-02 21:54:55 +0100

    Initial hunter spec

---
+++ test/hunter.spec.ts
@@ -0,0 +1,30 @@
+import * as chai from "chai";
+import * as mocha from "mocha";
+import { Boid } from "../src/creatures/boid";
+import { Creature } from "../src/creatures/creature";
+import { Hunter } from "../src/creatures/hunter";
+import { Vector2 } from "../src/vector2";
+
+const expect = chai.expect;
+
+describe("Hunter", () => {
+    describe("hunting vector", () => {
+        it("returns null if no prey in sight", () => {
+            const hunter = new Hunter(0, new Map(), () => {/**/});
+            const actual = hunter.huntingVector();
+            expect(actual).to.equal(null);
+        });
+
+        it("points towards nearest prey in sight", () => {
+            const creatures = new Map<number, Creature>();
+            const hunter = new Hunter(0, creatures, () => {/**/});
+            const boid = new Boid(1, creatures);
+            creatures.set(0, hunter);
+            creatures.set(1, boid);
+            hunter.position = new Vector2(1, 1);
+            boid.position = new Vector2(2, 3);
+
+            const actual = hunter.huntingVector();
+        });
+    });
+});
