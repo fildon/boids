c186cdefdc773fc01f846f7efb99cd987678747b
Rupert Foggo McKay <rumckay@expediagroup.com>
2020-10-03 11:25:39 +0100

    Minor fps counter improvements

--- src/ui/fpsCounter.ts
+++ src/ui/fpsCounter.ts
@@ -1,18 +1,18 @@
+const SECOND = 1000;
+
 export class FpsCounter {
   public static getFpsCounter(): FpsCounter {
     return this.fpsCounter || (this.fpsCounter = new FpsCounter());
   }
 
   private static fpsCounter: FpsCounter;
-  private readonly SECOND = 1000;
 
   private fpsLabel: HTMLElement;
-  private recentFrames: number[] = [];
-  private start: number;
+  private recentFrames: number[];
 
   private constructor() {
     this.fpsLabel = document.getElementById('fps-status')!;
-    this.start = performance.now();
+    this.recentFrames = [];
   }
 
   public countFrame(): void {
@@ -21,12 +21,9 @@ export class FpsCounter {
 
   public getFPS(): number {
     const currentTime = performance.now();
-    const multiplier = currentTime - this.start < this.SECOND ?
-      Math.floor(1000 / (currentTime - this.start)) :
-      1;
     return this.recentFrames.filter(
-      (drawTime: number) => drawTime >= currentTime - this.SECOND,
-    ).length * multiplier;
+      (drawTime: number) => drawTime >= currentTime - SECOND,
+    ).length;
   }
 
   public updateFps(): void {
