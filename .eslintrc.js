79214cdf07bf1ed8c3fa36496fa0ca156ac8e0d2
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-06-05 14:26:59 +0200

    Remove grunt and stryker

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:jest/style',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   root: true,
   rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-warning-comments': 'warn',
-    'quotes': ['error', 'single'],
-    'semi': 'error',
-  }
+    "comma-dangle": ["error", "always-multiline"],
+    "no-warning-comments": "warn",
+    semi: "error",
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  devtool: 'source-map',
-  entry: './src/entry.ts',
+  devtool: "source-map",
+  entry: "./src/entry.ts",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [ '.ts' ],
-    modules: [
-      path.resolve('./node_modules'),
-      path.resolve('./src'),
-    ],
+    extensions: [".ts"],
+    modules: [path.resolve("./node_modules"), path.resolve("./src")],
   },
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: path.resolve(),
   },
 };
