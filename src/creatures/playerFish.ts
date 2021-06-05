ab7b612bdda8679b004cabff8d039069ea78a78a
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-06-05 15:42:37 +0200

    Adds back player boid

---
+++ src/creatures/playerFish.ts
@@ -0,0 +1,32 @@
+import { Vector } from "../geometry/vector";
+import { InputHandler } from "../ui/inputHandler";
+import { Creature } from "./creature";
+import { config } from "../stateManagement/config";
+
+export default class PlayerFish extends Creature {
+  public colour = "black";
+  public size = 6;
+  public position: Vector;
+  public heading = 0;
+  public speed = 0;
+  public history: Vector[] = [];
+
+  constructor(public inputHandler: InputHandler) {
+    super();
+    this.position = new Vector(window.innerWidth / 2, window.innerHeight / 2);
+    for (let i = 0; i < config.creature.maxHistory; i++) {
+      this.history.push(this.position);
+    }
+  }
+
+  public update(): void {
+    this.updateHistory();
+    this.heading += this.inputHandler.getHeadingUpdate();
+    this.speed += this.inputHandler.getSpeedUpdate();
+    this.speed = Math.max(
+      Math.min(this.speed, config.player.maxSpeed),
+      config.player.minSpeed
+    );
+    this.position = this.position.add(this.velocity()).normalize();
+  }
+}
